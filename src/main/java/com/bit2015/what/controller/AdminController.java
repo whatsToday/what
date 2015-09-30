95ff181ffe4e209002cc69178c9989d5d93659cc
pjy <pjy@pjy-PC>
2015-09-30 10:45:32 +0900

    신기방기 신기방기

--- src/main/java/com/bit2015/what/controller/AdminController.java
+++ src/main/java/com/bit2015/what/controller/AdminController.java
@@ -1,5 +1,15 @@
 package com.bit2015.what.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+	
+	@RequestMapping()
+	public String index(){
+		return "/admin/index";
+	}
 	
 }

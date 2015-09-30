0a5bacb1aee474555278deaa2c7801db1d6897c3
pjy <pjy@pjy-PC>
2015-09-30 10:20:56 +0900

    dao 빈깡통 올린다 pjy

---
+++ src/main/java/com/bit2015/what/dao/FollowDao.java
@@ -0,0 +1,5 @@
+package com.bit2015.what.dao;
+
+public class FollowDao {
+
+}
--- src/main/java/com/bit2015/what/service/AdminService.java
+++ src/main/java/com/bit2015/what/service/AdminService.java
@@ -1,5 +1,12 @@
 package com.bit2015.what.service;
 
-public class AdminService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.bit2015.what.dao.CommentsDao;
+
+@Service
+public class AdminService {
+  @Autowired
+  CommentsDao commentDao;
 }

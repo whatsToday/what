73a3d105d12afe48b1ff2f125b3a04c9aa8d0703
pjy <pjy@pjy-PC>
2015-10-01 15:33:07 +0900

    goodcontent vo , goodpaln vo  수정 pjy

--- src/main/java/com/bit2015/what/vo/GoodContentVo.java
+++ src/main/java/com/bit2015/what/vo/GoodContentVo.java
@@ -1,57 +1,47 @@
 package com.bit2015.what.vo;
 
 public class GoodContentVo {
-	private long good_no;
+	private long goodContent_no;
 	private long content_no;
 	private long member_no;
 	private String memberName;
 	private String title;
-
-	public long getGood_no() {
-		return good_no;
+	public long getGoodContent_no() {
+		return goodContent_no;
 	}
-
-	public void setGood_no(long good_no) {
-		this.good_no = good_no;
+	public void setGoodContent_no(long goodContent_no) {
+		this.goodContent_no = goodContent_no;
 	}
-
 	public long getContent_no() {
 		return content_no;
 	}
-
 	public void setContent_no(long content_no) {
 		this.content_no = content_no;
 	}
-
 	public long getMember_no() {
 		return member_no;
 	}
-
 	public void setMember_no(long member_no) {
 		this.member_no = member_no;
 	}
-
 	public String getMemberName() {
 		return memberName;
 	}
-
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	@Override
 	public String toString() {
-		return "GoodContentVo [good_no=" + good_no + ", content_no="
-				+ content_no + ", member_no=" + member_no + ", memberName="
-				+ memberName + ", title=" + title + "]";
+		return "GoodContentVo [goodContent_no=" + goodContent_no
+				+ ", content_no=" + content_no + ", member_no=" + member_no
+				+ ", memberName=" + memberName + ", title=" + title + "]";
 	}
 
+	
 }
--- src/main/java/com/bit2015/what/vo/GoodPlanVo.java
+++ src/main/java/com/bit2015/what/vo/GoodPlanVo.java
@@ -1,57 +1,47 @@
 package com.bit2015.what.vo;
 
 public class GoodPlanVo {
-	private long good_no;
+	private long goodPlan_no;
 	private long plan_no;
 	private long member_no;
 	private String memberName;
 	private String planName;
-
-	public long getGood_no() {
-		return good_no;
+	public long getGoodPlan_no() {
+		return goodPlan_no;
 	}
-
-	public void setGood_no(long good_no) {
-		this.good_no = good_no;
+	public void setGoodPlan_no(long goodPlan_no) {
+		this.goodPlan_no = goodPlan_no;
 	}
-
 	public long getPlan_no() {
 		return plan_no;
 	}
-
 	public void setPlan_no(long plan_no) {
 		this.plan_no = plan_no;
 	}
-
 	public long getMember_no() {
 		return member_no;
 	}
-
 	public void setMember_no(long member_no) {
 		this.member_no = member_no;
 	}
-
 	public String getMemberName() {
 		return memberName;
 	}
-
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-
 	public String getPlanName() {
 		return planName;
 	}
-
 	public void setPlanName(String planName) {
 		this.planName = planName;
 	}
-
 	@Override
 	public String toString() {
-		return "GoodPlanVo [good_no=" + good_no + ", plan_no=" + plan_no
-				+ ", member_no=" + member_no + ", memberName=" + memberName
-				+ ", planName=" + planName + "]";
+		return "GoodPlanVo [goodPlan_no=" + goodPlan_no + ", plan_no="
+				+ plan_no + ", member_no=" + member_no + ", memberName="
+				+ memberName + ", planName=" + planName + "]";
 	}
 
+	
 }
